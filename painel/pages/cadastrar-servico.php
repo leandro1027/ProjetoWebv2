ac14321c4d6c38d8f4c1bddd3546ce5b0a2fe294
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-22 11:25:12 -0300

    Update cadastrar-servico.php

--- painel/pages/cadastrar-servico.php
+++ painel/pages/cadastrar-servico.php
@@ -1,18 +1,18 @@
 <div class="box-content">
-    <h2><i class="fas fa-plus"></i> Adicionar Serviço</h2>
+    <h2><i class="fas fa-plus"></i> Adicionar Competição</h2>
 
     <form method="post" enctype="multipart/form-data">
         <?php 
         if (isset($_POST['acao'])){
             if(Painel::insert($_POST)){
-            Painel::messageToUser('sucesso', 'Serviço cadastrado com sucesso!');
+            Painel::messageToUser('sucesso', 'Competição cadastrada com sucesso!');
             }else{
-                Painel::messageToUser('erro', 'Não foi possível cadastrar o serviço');
+                Painel::messageToUser('erro', 'Não foi possível cadastrar a competição');
             }
         }
         ?>
         <div class="form-group">
-            <label for="servico">Serviço: </label>
+            <label for="servico">Competição: </label>
             <textarea class="tinymce" name="servico" id=""><?php recoverPost('servico');?></textarea>
         </div>
        
